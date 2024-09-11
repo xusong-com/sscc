1e1460fd37ba73bd39b87bfda935c5e42e4f47a6
xusong.wang <xusong.wang@hotmail.com>
2024-09-11 15:17:11 +0900

    完全数判定機能追加

--- script.js
+++ script.js
@@ -45,3 +45,41 @@ function primeNumber(){
     }
 }
 console.log("xxx")
+
+function completeNumber(){
+    console.log("completeNumber start");
+    let N = inputText("txtCompleteNumber");
+
+    if (/^\d+$/.test(N)) {
+    } else {
+       showText("txtCompleteNumberResult","判定できません");
+       return;
+    }
+
+    console.log("N=", N);
+    N = Number(N)
+    if (N <= 0){
+        showText("txtCompleteNumberResult","判定できません");
+        return;
+    }
+    let S = 0;
+    let T = 1;
+    let U = N;
+    let Q = 0;
+    let P = 0;
+
+    while (2 * T <= N){
+        if (T * U == N){
+            S = S + T;
+        }
+        T = T + 1;
+        Q = N % T;
+        P = N - Q;
+        U = P / T;
+    }
+    if (S == N){
+        showText("txtCompleteNumberResult","完全数です");
+    }else{
+        showText("txtCompleteNumberResult","完全数ではないです");
+    }
+}
